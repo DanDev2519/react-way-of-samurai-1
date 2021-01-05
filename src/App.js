18c89fc0a4aa69cf9a2463d837508a924a5bdd56
DanDev2519 <ostryakov.danil@yandex.ru>
2021-01-05 18:28:23 +0300

    Add route-dom and stubs of other pages

--- src/App.js
+++ src/App.js
@@ -2,19 +2,30 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
-// import Profile from "./components/Profile/Profile";
+import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import { BrowserRouter, Route } from "react-router-dom";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
+import News from "./components/News/News";
 
 const App = () => {
   return (
-    <div className="app__wrapper">
-      <Header />
-      <Navbar />
-      <div className="app__content">
-        {/* <Profile /> */}
-        <Dialogs />
+    <BrowserRouter>
+      <div className="app__wrapper">
+        <Header />
+        <Navbar />
+        <div className="app__content">
+          <Route path="/Profile" component={Profile} />
+          <Route path="/Dialogs" component={Dialogs} />
+          <Route path="/News" component={News} />
+          <Route path="/Music" component={Music} />
+          <Route path="/Settings" component={Settings} />
+          {/* <Profile /> */}
+          {/* <Dialogs /> */}
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 };
 
