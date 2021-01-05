d2b39c3ceed97778372e55350e146f80ce1daea2
DanDev2519 <ostryakov.danil@yandex.ru>
2021-01-05 17:44:12 +0300

    Commenting on unused code

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,11 +5,11 @@ const Navbar = () => {
 	return (
 		<nav className={classes.nav}>
 		<ul>
-			<li className={`${classes.item} ${classes.active}`}><a>Profile</a></li>
-			<li className={classes.item}><a>Messages</a></li>
-			<li className={classes.item}><a>News</a></li>
-			<li className={classes.item}><a>Music</a></li>
-			<li className={classes.item}><a>Settings</a></li>
+			<li className={`${classes.item} ${classes.active}`}><a href="#1">Profile</a></li>
+			<li className={classes.item}><a href="#1">Messages</a></li>
+			<li className={classes.item}><a href="#1">News</a></li>
+			<li className={classes.item}><a href="#1">Music</a></li>
+			<li className={classes.item}><a href="#1">Settings</a></li>
 		</ul>
 	</nav>
 )
