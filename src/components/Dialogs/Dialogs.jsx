830c639783c3b59261ffc8d6dc7cfc7de7e64673
DanDev2519 <ostryakov.danil@yandex.ru>
2021-01-07 14:07:57 +0300

    Create maps for Dialogs and MyPosts

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -13,27 +13,39 @@ const DailogItem = (props) => {
 };
 
 const Message = (props) => {
-  return (
-    <div className={s.message}>{props.text}</div>
-  );
+  return <div className={s.message}>{props.text}</div>;
 };
 
 const Dialogs = () => {
+  let dialogsData = [
+    { id: 1, name: "Jon" },
+    { id: 2, name: "Ben" },
+    { id: 3, name: "Sara" },
+    { id: 4, name: "Anna" },
+    { id: 5, name: "Den" }
+  ];
+
+  let messagesData = [
+    { id: 1, messages: "Hi" },
+    { id: 2, messages: "How are you?" },
+    { id: 3, messages: "Good!" }
+  ];
+
   return (
     <div>
       <h2>Dialogs</h2>
       <div className={s.dialogs}>
         <div className={s.dialogItems}>
-          <DailogItem name="Jon" id="1" />
-          <DailogItem name="Ben" id="2" />
-          <DailogItem name="Sara" id="3" />
-          <DailogItem name="Anna" id="4" />
-          <DailogItem name="Den" id="5" />
+          <DailogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+          <DailogItem name={dialogsData[1].name} id={dialogsData[1].id} />
+          <DailogItem name={dialogsData[2].name} id={dialogsData[2].id} />
+          <DailogItem name={dialogsData[3].name} id={dialogsData[3].id} />
+          <DailogItem name={dialogsData[4].name} id={dialogsData[4].id} />
         </div>
         <div className={s.messages}>
-          <Message text="Hi"/>
-          <Message text="How are you?"/>
-          <Message text="Good!"/>
+          <Message text={messagesData[0].messages} />
+          <Message text={messagesData[1].messages} />
+          <Message text={messagesData[2].messages} />
         </div>
       </div>
     </div>
