1c557be74e59d657d60a92b9998f92acbfdccb42
DanDev2519 <ostryakov.danil@yandex.ru>
2021-01-21 00:25:33 +0300

    Lesson-55: Add pagination

--- src/components/Users/UsersC.jsx
+++ src/components/Users/UsersC.jsx
@@ -18,14 +18,53 @@ class Users extends React.Component {
   // };
   componentDidMount() {
     axios
-    .get("https://social-network.samuraijs.com/api/1.0/users")
-    .then((response) => {
-      this.props.setUsers(response.data.items);
-    });
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users&page=${this.props.currentPage}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+        this.props.setTotalUsersCount(response.data.totalCount);
+      });
   }
+  onPageChanged = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users&page=${pageNumber}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+      });
+  };
   render() {
+    let pagesCount = Math.ceil(
+      this.props.totalUsersCount / this.props.pageSize
+    );
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+      pages.push(i);
+    }
+
     return (
       <div>
+        <ul className={style.breadCrumbs}>
+          {pages.map((p) => {
+            return (
+              <li
+                className={`${style.crumb} ${
+                  this.props.currentPage === p ? style.currentCrumb : ""
+                }`}
+                onClick={() => {
+                  this.onPageChanged(p);
+                }}
+              >
+                {p}
+              </li>
+            );
+          })}
+
+          {/* <li className={`${style.crumb} ${style.currentCrumb}`}>1</li> */}
+        </ul>
         {/* <button onClick={this.getUsers}>Get users</button> */}
         {this.props.users.map((u) => (
           <div>
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,6 +1,8 @@
 const FOLLOW = "FOLLOW";
 const UNFOLLOW = "UNFOLLOW";
 const SET_USERS = "SET_USERS";
+const SET_CURRENT_PAGE = "SET_CURRENT_PAGE";
+const SET_TOTAL_USERS_COUNT = "SET_TOTAL_USERS_COUNT";
 
 let initialState = {
   users: [
@@ -37,6 +39,10 @@ let initialState = {
     //   location: { country: "Ukraine", city: "Kiev" },
     // },
   ],
+  pageSize: 4,
+  // totalUsersCount: 0,
+  totalUsersCount: 10,
+  currentPage: 1,
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -44,7 +50,20 @@ const usersReducer = (state = initialState, action) => {
     case SET_USERS:
       return {
         ...state,
-        users: [...state.users, ...action.users],
+        // users: [...state.users, ...action.users],
+        users: action.users,
+      };
+
+    case SET_CURRENT_PAGE:
+      return {
+        ...state,
+        currentPage: action.currentPage,
+      };
+
+    case SET_TOTAL_USERS_COUNT:
+      return {
+        ...state,
+        totalUsersCount: action.count,
       };
 
     case FOLLOW:
@@ -79,5 +98,13 @@ const usersReducer = (state = initialState, action) => {
 export const followAC = (userID) => ({ type: FOLLOW, userID });
 export const unfollowAC = (userID) => ({ type: UNFOLLOW, userID });
 export const setUsersAC = (users) => ({ type: SET_USERS, users });
+export const setCurrentPageAC = (currentPage) => ({
+  type: SET_CURRENT_PAGE,
+  currentPage,
+});
+export const setTotalUsersCountAC = (totalUsersCount) => ({
+  type: SET_TOTAL_USERS_COUNT,
+  count: totalUsersCount,
+});
 
 export default usersReducer;
