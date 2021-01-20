d16731204ec7c2e29f6afe3d5fb5498e6c5d2daa
DanDev2519 <ostryakov.danil@yandex.ru>
2021-01-20 20:22:20 +0300

    Lesson-53: Create class component UsersC.jsx

---
+++ src/components/Users/UsersC.jsx
@@ -0,0 +1,75 @@
+import * as axios from "axios";
+import React from "react";
+import style from "./Users.module.css";
+import avatarExample from "../../assets/img/avatar-empty.png";
+
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+      });
+  }
+  // getUsers = () => {
+  //   if (this.props.users.length === 0) {
+  //     axios
+  //       .get("https://social-network.samuraijs.com/api/1.0/users")
+  //       .then((response) => {
+  //         this.props.setUsers(response.data.items);
+  //       });
+  //   }
+  // };
+  render() {
+    return (
+      <div>
+        {/* <button onClick={this.getUsers}>Get users</button> */}
+        {this.props.users.map((u) => (
+          <div>
+            <div>
+              <div>
+                <img
+                  className={style.ava}
+                  src={u.photos.small != null ? u.photos.small : avatarExample}
+                  alt="Users ava"
+                />
+              </div>
+              <div>
+                {u.followed ? (
+                  <button
+                    onClick={() => {
+                      this.props.unfollow(u.id);
+                    }}
+                  >
+                    Unfollow
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => {
+                      this.props.follow(u.id);
+                    }}
+                  >
+                    Follow
+                  </button>
+                )}
+              </div>
+            </div>
+            <div>
+              <div>
+                <p>{u.fullName}</p>
+                <p>{u.status}</p>
+              </div>
+              <div>
+                <p>{"u.location.country"}</p>
+                <p>{"u.location.city"}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { followAC, setUsersAC, unfollowAC } from "../../redux/users-reducer";
 import Users from "./Users";
+import UsersC from "./UsersC";
 
 let mapStateToProps = (state) => {
 	return {
@@ -23,6 +24,6 @@ let mapDispatchToProps = (dispatch) => {
 	};
 };
 
-const UsersUsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
+const UsersUsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersC);
 
 export default UsersUsersContainer;
